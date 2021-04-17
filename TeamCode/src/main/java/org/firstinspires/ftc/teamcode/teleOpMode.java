6c8a1ae26acfb55a0f8535aa946532cb14704c37
Ethan Dessner <55603327+ethandessner@users.noreply.github.com>
2021-04-17 14:03:31 -0400

    Latest Auto and Tele code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
@@ -3,19 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class teleOpMode extends OpMode {
 
-    DcMotor frontLeft, frontRight, backLeft, backRight, tread, fire;
-
+    DcMotor frontLeft, frontRight, backLeft, backRight, tread, fire, twist;
+    Servo wobble;
+ //   Servo.Direction forward, back;
+//    ServoController wobble;
     double power = 1.0;
     boolean toggle = true;
     int tap = 0;
 //    boolean start = false;
     double power2 = 1.0;
     double powerFire = 0.0;
-
+    double tor = 0.8;
+//servo port zero expansion
+    //twist port 2 control
     @Override
     public void init() {
         frontLeft = hardwareMap.dcMotor.get("Front Left");
@@ -24,6 +29,11 @@ public class teleOpMode extends OpMode {
         backRight = hardwareMap.dcMotor.get("Back Right");
         tread = hardwareMap.dcMotor.get("Tread");
         fire = hardwareMap.dcMotor.get("Fire");
+        wobble = hardwareMap.servo.get("Wobble");
+        wobble.setPosition(0);
+
+      //  wobble = hardwareMap.servoController.get("Wobble");
+        twist = hardwareMap.dcMotor.get("Twist");
 //        start = true;
 //        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
 //        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -38,12 +48,35 @@ public class teleOpMode extends OpMode {
             backRight.setPower(gamepad1.right_stick_y * -power);
             fire.setPower(0);
             tread.setPower(0);
+    //        twist.setPower(gamepad1.left_trigger * -tor);
+      //      twist.setPower(gamepad1.right_trigger * tor);
+       //     twist.setPower(0);
+       //     wobble.setPosition(0.0);
+        if(gamepad1.left_stick_button){
+            tor = .7;
+        }
+        if(gamepad1.right_stick_button) {
+            tor = .8;
+        }
+
+        if(gamepad1.dpad_right){
+            twist.setPower(tor);
+        }
+        if(gamepad1.dpad_left){
+            twist.setPower(-tor);
+        }
+        if(gamepad1.y){
+            wobble.setPosition(-1);
+        }
+        if(gamepad1.x){
+            wobble.setPosition(1);
+        }
 
         if (gamepad2.right_bumper) {
-            fire.setPower(powerFire);
+            fire.setPower(-powerFire);
         }
         if(gamepad2.left_bumper){
-            fire.setPower(-powerFire);
+            fire.setPower(powerFire);
         }
         if (gamepad2.y) {
             tread.setPower(-power2);
@@ -101,7 +134,7 @@ public class teleOpMode extends OpMode {
             power2 = 0.7;
         }
         if (gamepad2.x) {
-            power2 = 0.5;
+            power2 = 1.0;
         }
         if (gamepad1.b) {
             power = 0.7;
@@ -145,6 +178,7 @@ public class teleOpMode extends OpMode {
         telemetry.addData("Current Toggle Value for Ethan: ", power);
         telemetry.addData("Current Toggle Value for Alicia: ", power2);
         telemetry.addData("Current Toggle Value for Fire Power: ", powerFire);
+        telemetry.addData("Servo position", wobble.getPosition());
 
     }
 
